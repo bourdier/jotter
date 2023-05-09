65be207eaf678fa61cb9acc6150704f1b1d78e56
Théo Bourdier <theobourdier1@gmail.com>
2023-05-09 21:26:21 +0200

    Create Dashboard & link to router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './routes/Homepage';
+import Dashboard from './routes/Dashboard';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="login" element={<Dashboard />} />
       </Routes>
     </>
   );
