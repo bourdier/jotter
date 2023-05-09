1337e762438844163c9feb8c993be7c1c16915a2
Théo Bourdier <theobourdier1@gmail.com>
2023-05-09 21:42:51 +0200

    Remove bullet list

--- src/components/Navtab.js
+++ src/components/Navtab.js
@@ -6,19 +6,19 @@ export default function Navtab() {
         <input type="text" id="searchbox" placeholder="Search notes" />
       </div>
       <div className="navtab__tools">
-        <ul>
+        <ul className="no-bullet">
           <li><button className="no"><i className="fa-solid fa-fw fa-square-plus"></i>New notes</button></li>
           <li><button className="no"><i className="fa-solid fa-fw fa-folder-plus"></i>New folder</button></li>
           <li><button className="no"><i className="fa-solid fa-fw fa-file-import"></i>Import notes</button></li>
         </ul>
       </div>
       <div className="navtab__folders">
-        <ul>
+        <ul className="no-bullet">
           <li><button className="no"><i className="fa-solid fa-fw fa-folder"></i>Folder 1</button></li>
         </ul>
       </div>
       <div className="navtab__footer">
-        <ul>
+        <ul className="no-bullet">
           <li><button className="no"><i className="fa-solid fa-fw fa-gear"></i>Settings</button></li>
           <li><button className="no"><i className="fa-solid fa-fw fa-arrow-right-from-bracket"></i>Log out</button></li>
         </ul>
