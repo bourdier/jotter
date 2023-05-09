a5cdfae7dead0c8b43df6efb019411a8b6113d31
Théo Bourdier <theobourdier1@gmail.com>
2023-05-09 20:26:05 +0200

    Init React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import './index.css';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    
+    </>
   );
 }
 
