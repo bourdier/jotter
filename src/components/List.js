d8b08900fae557b15c892b7824709678a4bac6bb
Théo Bourdier <theobourdier1@gmail.com>
2023-05-10 15:59:13 +0200

    Delete React

--- src/components/List.js
+++
@@ -1,8 +0,0 @@
-export default function List() {
-  return (
-    <>
-      Note 1
-      Note 2
-    </>
-  )
-}
--- src/routes/Home.js
+++
@@ -1,9 +0,0 @@
-import Dashboard from "../components/Dashboard"
-
-export default function Home() {
-  return (
-    <Dashboard>
-      <a href="/note">test</a>
-    </Dashboard>
-  )
-}
