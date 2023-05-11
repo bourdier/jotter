1783545c827676048ac8cc1a9cf0b70f352c9fb0
Théo Bourdier <theobourdier1@gmail.com>
2023-05-11 13:09:43 +0200

    Add edit btn

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,4 +1,6 @@
-export default function EditorTools({ editor }) {
+export default function EditorTools() {
+
+  let editor = false;
 
   const redirection = (path) => {
     window.location = path
@@ -31,6 +33,12 @@ export default function EditorTools({ editor }) {
               <i className="fa-solid fa-fw fa-trash"></i>Delete
             </button>
           </li>
+          <li>
+            <button className="no tools"><i className="fa-solid fa-fw fa-gear no-text"></i></button>
+          </li>
+          <li>
+            <button className="no tools"><i className="fa-solid fa-fw fa-arrow-right-from-bracket no-text"></i></button>
+          </li>
         </ul>
       </div>
       <div className="notes">
@@ -43,6 +51,11 @@ export default function EditorTools({ editor }) {
           <i className="fa-solid fa-fw fa-arrow-left"></i>Go back
         </button>
         <ul className="upperbar__tools">
+        <li>
+            <button className="no tools">
+              <i className="fa-solid fa-fw fa-pen-to-square"></i>Edit
+            </button>
+          </li>
           <li>
             <button className="no tools">
               <i className="fa-solid fa-fw fa-file-arrow-down"></i>Export
@@ -52,6 +65,12 @@ export default function EditorTools({ editor }) {
             <button className="no tools">
               <i className="fa-solid fa-fw fa-trash"></i>Delete
             </button>
+          </li>
+          <li>
+            <button className="no tools"><i className="fa-solid fa-fw fa-gear no-text"></i></button>
+          </li>
+          <li>
+            <button className="no tools"><i className="fa-solid fa-fw fa-arrow-right-from-bracket no-text"></i></button>
           </li>
         </ul>
       </div>
--- src/routes/Editor.jsx
+++ src/routes/Editor.jsx
@@ -1,11 +1,9 @@
 import Tools from "../components/Tools";
 
 export default function Dashboard() {
-  let editor = false;
-
   return (
     <div className="dashboard">
-      <Tools editor={editor} />      
+      <Tools/>      
       <div className="dashboard__content">
 
       </div>
