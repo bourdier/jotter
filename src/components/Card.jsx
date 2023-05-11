9bec343ee9236751b8774a72265b7f2d89ed9558
Théo Bourdier <theobourdier1@gmail.com>
2023-05-11 12:09:36 +0200

    Create card note

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+export default function Card() {
+  return (
+    <div className="card">
+      <div className="card__header">
+        <h2>Card title</h2>
+      </div>
+      <div className="card__content">
+        <p>Card content</p>
+      </div>
+    </div>
+  )
+}
