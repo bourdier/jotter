b34b00db7cd5a3316a3cfe02fc118897ad8a15eb
Théo Bourdier <theobourdier1@gmail.com>
2023-05-11 16:50:42 +0200

    Remove Hash from the Markdown title

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Markdown from "marked-react";
 
 export default function Card({ data }) {
 
@@ -6,17 +7,26 @@ export default function Card({ data }) {
     window.location = `/notes/${note.id}`
   }
 
+  // if h2 contain # it will be removed function
+  const removeHash = (str) => {
+    if (str.charAt(0) === '#') {
+      return str.slice(1);
+    } else {
+      return str;
+    }
+  }
+
   return (
     <>
     {data?.map((note) => 
       <div className="card" key={note.id} onClick={() => {redirectTo(note)}}>
         <div className="card__header">
-          <h2>{note.title}</h2>
+          <h2>{removeHash(note.title)}</h2>
         </div>
         <div className="card__content">
-          <p>
+          <Markdown>
             {note.content}
-          </p>
+          </Markdown>
         </div>
         <div className="card__tags__list">
           <ul className="no-bullet">
