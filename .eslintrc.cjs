bd355e9cb095f441425a5e9cbfeb96887becbd7d
Théo Bourdier <theobourdier1@gmail.com>
2023-05-12 11:05:24 +0200

    Recommended requiring type checking

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
